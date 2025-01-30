d800c61ff60f3175dca757a37a33560a1df4e9a9
apcmmbalidio <mmbalidio@student.apc.edu.ph>
2025-01-30 22:36:35 +0800

    second component

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,11 @@
-<template>
-  <h1>Food</h1>
-  <food-item/> <br>
-  <food-item2/> <br>
-  <food-item/> <br>
-  <food-item2/> <br>
-</template>
-
 <script></script>
+
+<template>
+   <h1>Food</h1>
+  <food-item/> 
+  <food-item2/>
+  <food-item/> 
+  <food-item2/> 
+</template>
 
 <style></style>
--- vue-project/src/components/FoodItem2.vue
+++ vue-project/src/components/FoodItem2.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="mango">
-    <h2>{{ name }}</h2>
-    <p>{{ message }}</p>
-  </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      name: 'Mangoes',
-      message: 'I like Mangoes'
+    <div class="apple">
+      <h2>{{ name }}</h2>
+      <p>{{ message }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        name: 'Apples',
+        message: 'I like apples'
+      }
     }
-  }
-};
-</script>
-
-<style>
-    .mango{
+  };
+  </script>
+  
+  <style>
+    .apple {
     border: dashed black 1px;
     display: inline-block;
     margin: 10px;
     padding: 10px;
-    background-color: lightyellow;
+    background-color: lightgreen;
   }
 </style>
