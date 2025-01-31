c954238d233b19162f268f07d23c497b84a3c554
apcmmbalidio <mmbalidio@student.apc.edu.ph>
2025-01-31 08:18:36 +0800

    commit

---
+++ vue-project/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://ironiqfaybfawjoqerju.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlyb25pcWZheWJmYXdqb3Flcmp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyNTI5OTcsImV4cCI6MjA1MzgyODk5N30.gCQPNW2UncTFLukoqIxXjJnNJBtIE8nr_3dx9WsfDbQ')
